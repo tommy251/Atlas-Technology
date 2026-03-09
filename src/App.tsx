1fae2bef716e70d688b9ca00b39940f2de73a112
Tomiwa Babatunde <tommybab7@gmail.com>
2026-03-09 05:58:27 -0700

    error

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "@/context/CartContext";
+import { WishlistProvider } from "@/context/WishlistContext";
 import { AuthProvider } from "@/context/AuthContext";
+import CartDrawer from "@/components/CartDrawer";
 import AuthModal from "@/components/AuthModal";
+import WhatsAppButton from "@/components/WhatsAppButton";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import CategoryPage from "./pages/CategoryPage";
@@ -12,31 +16,40 @@ import OpenBoxPage from "./pages/OpenBoxPage";
 import PreOwnedPage from "./pages/PreOwnedPage";
 import SupportPage from "./pages/SupportPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
+import WishlistPage from "./pages/WishlistPage";
+import AllProductsPage from "./pages/AllProductsPage";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/category/:categorySlug" element={<CategoryPage />} />
-            <Route path="/category/:categorySlug/:subcategorySlug" element={<CategoryPage />} />
-            <Route path="/open-box" element={<OpenBoxPage />} />
-            <Route path="/pre-owned" element={<PreOwnedPage />} />
-            <Route path="/support" element={<SupportPage />} />
-            <Route path="/product/:productId" element={<ProductDetailPage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-          <AuthModal />
-        </BrowserRouter>
-      </TooltipProvider>
-    </AuthProvider>
+    <TooltipProvider>
+      <AuthProvider>
+        <CartProvider>
+          <WishlistProvider>
+            <Toaster />
+            <Sonner />
+            <BrowserRouter>
+              <CartDrawer />
+              <AuthModal />
+              <WhatsAppButton />
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/category/:categorySlug" element={<CategoryPage />} />
+                <Route path="/category/:categorySlug/:subcategorySlug" element={<CategoryPage />} />
+                <Route path="/open-box" element={<OpenBoxPage />} />
+                <Route path="/pre-owned" element={<PreOwnedPage />} />
+                <Route path="/support" element={<SupportPage />} />
+                <Route path="/product/:productId" element={<ProductDetailPage />} />
+                <Route path="/wishlist" element={<WishlistPage />} />
+                <Route path="/shop" element={<AllProductsPage />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </BrowserRouter>
+          </WishlistProvider>
+        </CartProvider>
+      </AuthProvider>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
